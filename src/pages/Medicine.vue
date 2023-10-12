5c17ee0894815f422940e97d9dde00bf3c20d4ee
bugradostt <bugradostt@gmail.com>
2023-10-12 13:49:22 +0300

    delete dizayn

--- src/pages/Medicine.vue
+++ src/pages/Medicine.vue
@@ -1,7 +1,44 @@
-<template></template>
+<template>
+  <div class="card-margin">
+    <q-layout view="lHh lpr lFf" container style="height: 780px; margin-top: 10px;" class=" rounded-borders">
+      <q-header bordered class="bg-grey-3 text-primary">
+        <q-toolbar>
+          <q-toolbar-title class="text-center">
+            <q-avatar color="blue" text-color="white" icon="fa-solid fa-capsules" />
+            İlaç Listesi
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+
+
+      <q-page-container>
+        <div class="row">
+          <div class="col q-ma-md">
+            <q-btn color="blue" text-color="white" unelevated icon="fa-solid fa-plus" label="Ekle" />
+          </div>
+          <div class="col q-ma-md">
+            <q-input outlined v-model="text" label="İlaç arayınız" />
+          </div>
+        </div>
+
+
+
+
+
+      </q-page-container>
+
+    </q-layout>
+  </div>
+</template>
 
 
 <script setup>
+
+import { ref } from 'vue'
+
+const blueModel = ref(true)
+const pinkModel = ref('Aktif')
 
 </script>
 
