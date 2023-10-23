16583cfd577ab10f523d328948458eca72542cd3
bugradostt <bugradostt@gmail.com>
2023-10-23 17:04:12 +0300

    modal dizayn

--- src/pages/EducationType.vue
+++ src/pages/EducationType.vue
@@ -2,6 +2,7 @@
   <page-container title="Eğitim Türleri">
     <template #icon>
       <i class="fa-solid fa-swatchbook"></i>
+
     </template>
 
     <q-page-container>
@@ -38,7 +39,7 @@
                 <i class="fa-solid fa-trash-can fa-lg"></i>
               </button>
 
-              <button class="btn" style="border:none;" title="Yapıldı/Yapılmadı">
+              <button class="btn" style="border:none;" title="Detay" @click="detailEducationalPlanModal(props.row)">
                 <i class="fa-solid fa-eye fa-lg"></i>
               </button>
             </q-td>
@@ -57,79 +58,147 @@
 
   <!-- Add and Update Modal Start -->
   <!-- :positive="currentItem.id ? 'Güncelle' : 'Ekle'"
-
   v-model="currentItem.type" :virtual-scroll-horizontal="false"
   -->
-  <form-modal-dialog v-model="addUpdateDialog" title="İlaç Bilgisi" negative="Kapat" @positive="updateCommit()">
-
-
-
-    <div class="input-group">
-      <q-input class="txt" filled type="date" v-model="dataItem.planningDate" label="Planlanma Tarihi" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
-    </div>
+  <form-modal-dialog v-model="addUpdateDialog" title="Eğitim Tür Bilgisi" icon="fa-solid fa-school-circle-check"
+    negative="Kapat" @positive="updateCommit()">
 
     <div class="input-group">
       <q-input class="txt" v-model="dataItem.educationName" filled label="Eğitim Adı" lazy-rules
         :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
     </div>
 
-    <div class="input-group">
-      <q-select class="select" v-model="dataItem.educationOrganization" filled :virtual-scroll-horizontal="false"
-        :options="['Seçiniz', '43', '33', '1']" label="Eğitim Veren Kurum" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
-    </div>
 
     <div class="input-group">
-      <q-select class="select" v-model="dataItem.educationType" filled :virtual-scroll-horizontal="false"
-        :options="['Seçiniz', '43', '33', '1']" label="Eğitim Türü" lazy-rules
+      <q-input class="txt" v-model="dataItem.aim" filled type="textarea" label="Amaç" lazy-rules
         :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
     </div>
 
 
     <div class="input-group">
-      <q-toggle v-model="dataItem.exam" label="Sınav" />
+      <q-toggle v-model="dataItem.compulsory" label="Zorunlu" />
     </div>
 
+    <hr>
+    <label style="font-size: 18px; color: gray;">Zorunlu Eğitim Görevleri</label>
 
-    <div class="input-group">
-      <q-input class="txt" v-model="dataItem.successPoint" filled type="number" label="Başarı Puanı" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
+    <div>
+
+
+      <div style="margin-top: 15px;">
+        <q-card class="my-card" flat bordered>
+          <q-card-actions @click="btnTask = !btnTask" style="cursor: pointer;">
+            <q-btn flat color="primary" :label="editAddBtn == 'add' ? 'Görev Ekle' : 'Görev Güncelle'" />
+
+            <q-space />
+
+            <q-btn color="grey" round flat dense :icon="btnTask ? 'keyboard_arrow_up' : 'keyboard_arrow_down'" />
+          </q-card-actions>
+
+          <q-slide-transition>
+            <div v-show="btnTask">
+              <q-separator />
+              <q-card-section class="text-subtitle2">
+                <div class="row">
+
+                  <div class="col-md-8 col-xs-12">
+                    <div class="input-group">
+                      <q-select filled v-model="dataItem.taskName" :options="['A', 'B', 'C']" label="Görev Seçiniz" />
+                    </div>
+                  </div>
+
+                  <div class="col-md-4"
+                    style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100%;">
+
+                    <!-- add -->
+                    <q-btn v-if="editAddBtn == 'add'" color="primary" icon="fa-solid fa-plus" label="Ekle"
+                      @click="AddDataTask(dataItem.taskName)" />
+
+                    <!-- edit -->
+                    <q-btn v-if="editAddBtn == 'edit'" color="warning" icon="fa-solid fa-pen" label="Güncelle"
+                      @click="EditDataTask(dataItem.taskName)" />
+
+
+
+                  </div>
+                </div>
+
+              </q-card-section>
+            </div>
+          </q-slide-transition>
+        </q-card>
+      </div>
+
+      <!--
+
+      <span v-if="btnTask == false">
+        <q-btn color="green" @click="btnTask = !btnTask" icon="fa-solid fa-plus" style="margin: 5px 0 10px 0;" />
+      </span>
+      <span v-else>
+        <q-btn color="red" @click="btnTask = !btnTask" icon="fa-solid fa-minus" style="margin: 5px 0 10px 0;" />
+      </span>
+      <q-slide-transition>
+        <div v-show="btnTask">
+
+          <div class="row">
+
+            <div class="col-md-8 col-xs-12">
+              <div class="input-group">
+                <q-input class="txt" v-model="dataItem.taskName" filled label="Görev Adı" lazy-rules
+                  :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
+              </div>
+            </div>
+
+            <div class="col-md-4"
+              style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100%;">
+
+               add
+              <q-btn v-if="editAddBtn == 'add'" color="primary" icon="fa-solid fa-plus" label="Ekle"
+                @click="AddDataTask(dataItem.taskName)" />
+
+               edit
+              <q-btn v-if="editAddBtn == 'edit'" color="warning" icon="fa-solid fa-pen" label="Güncelle"
+                @click="EditDataTask(dataItem.taskName)" />
+
+
+
+            </div>
+
+          </div>
+
+        </div>
+      </q-slide-transition> -->
+
+
+
     </div>
 
+    <q-markup-table v-if="dataTask.length != 0" style="box-shadow: none; margin-top: 10px;">
+      <thead>
+        <tr class="bg-light">
+          <th class="text-left"></th>
+          <th class="text-left">Görev Adı</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(i, index) in  dataTask " :key="index" :class="index % 2 == 1 ? 'bg-light' : ''">
 
-    <div class="input-group">
-      <q-input class="txt" v-model="dataItem.validityDate" filled type="date" label="Geçerlilik Tarihi" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
-    </div>
+          <td class="text-left" style="width: 5px;">
+            <i class="fa-solid fa-pen-to-square fa-lg" @click="EditDataTaskModel(index, i)"
+              style="margin-right: 10px; cursor: pointer;"></i>
+            <i style="cursor: pointer;" class="fa-solid fa-trash fa-lg" @click="DeleteDataTask(index)"></i>
+          </td>
+          <td class="text-left">{{ i }} </td>
 
-    <div class="input-group">
-      <q-input class="txt" v-model="dataItem.compulsoryTimeMin" filled label="Zorunlu Süre" mask="###/##" fill-mask
-        hint="Saat/Dakika" :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
-    </div>
+        </tr>
 
-    <div class="input-group">
-      <q-select class="select" v-model="dataItem.planningDate" filled :virtual-scroll-horizontal="false"
-        :options="['Seçiniz', '43', '33', '1']" label="Personel Seçiniz" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
-    </div>
 
-    <div class="input-group">
-      <q-select class="select" v-model="dataItem.planningDate" filled :virtual-scroll-horizontal="false"
-        :options="['Seçiniz', '43', '33', '1']" label="Grup Seçiniz" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
-    </div>
+      </tbody>
+    </q-markup-table>
 
-    <div class="input-group">
-      <q-select class="select" v-model="dataItem.planningDate" filled :virtual-scroll-horizontal="false"
-        :options="['Seçiniz', '43', '33', '1']" label="Bölüm Seçiniz" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
-    </div>
-
-    <div class="input-group">
-      <q-select class="select" v-model="dataItem.planningDate" filled :virtual-scroll-horizontal="false"
-        :options="['Seçiniz', '43', '33', '1']" label="Görev Seçiniz" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Boş geçilemez']" />
+    <div v-else class="data-task-alert">
+      <i class="fa-solid fa-circle-exclamation fa-xl" style="margin-right: 10px;"></i>
+      Görev bulunamadı. Lüften yeni görev ekleyiniz!
     </div>
 
 
@@ -140,6 +209,72 @@
 
   </form-modal-dialog>
   <!-- Add and Update Modal End -->
+
+
+
+  <!-- Detail Modal Start -->
+  <form-modal-dialog v-model="detailDialog" title="Eğitim Tür Detay Bilgisi" icon="fa-solid fa-school-circle-check"
+    negative="Kapat" @positive="updateCommit()">
+
+
+    <q-markup-table style="box-shadow: none; margin-top: 10px;">
+      <thead>
+        <tr class="bg-light">
+          <th class="text-left">Eğitim Adı</th>
+          <th class="text-left">Amaç</th>
+          <th class="text-left">Durumu</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+
+          <td class="text-left">{{ dataItem.educationName }}</td>
+          <td class="text-left">{{ dataItem.aim }}</td>
+          <td class="text-left" v-if="dataItem.compulsory" style="color: green;">Zorunlu</td>
+          <td class="text-left" v-else style="color: red;">Zorunlu Değil</td>
+
+        </tr>
+
+
+      </tbody>
+    </q-markup-table>
+
+
+    <hr>
+    <label style=" font-size: 18px; color: gray;">Zorunlu Eğitim Görevleri</label>
+
+
+
+    <q-markup-table v-if="dataTask.length != 0" style="box-shadow: none; margin-top: 10px;">
+      <thead>
+        <tr class="bg-light">
+          <th class="text-left">Görev Adı</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(i, index) in  dataTask " :key="index" :class="index % 2 == 1 ? 'bg-light' : ''">
+
+          <td class="text-left">{{ i }} </td>
+
+        </tr>
+
+
+      </tbody>
+    </q-markup-table>
+
+    <div v-else class="data-task-alert">
+      <i class="fa-solid fa-circle-exclamation fa-xl" style="margin-right: 10px;"></i>
+      Görev eklenmemiştir!
+    </div>
+
+
+
+
+
+
+
+  </form-modal-dialog>
+  <!-- Detail Modal End -->
 
   <!-- Delete modal start -->
   <form-modal-dialog v-model="deleteDialog" title="Eğitim Silme Onayı" @positive="del()" positive="Sil" negative="Kapat"
@@ -164,7 +299,7 @@ const columns = [
   { name: 'educationName', label: 'Eğitim Adı', align: 'left', field: 'educationName', sortable: true },
   {
     name: 'aim', label: 'Amaç', align: 'left', field: 'aim',
-    classes: "classes",
+    classes: "aim-width",
     "col-props": { style: 'width: 100px' },
     sortable: true
   },
@@ -175,17 +310,90 @@ const columns = [
   },
 ]
 
+const dataTask = ref([])
+const editAddBtn = ref("add")
+
 /* add modal start */
-const dataItem = ref({
-  exam: false
+var dataItem = ref({
+  exam: false,
+  compulsory: false
 });
 
 const addUpdateDialog = ref(false);
+const detailDialog = ref(false);
+const btnTask = ref(false);
 
 const addEducationalPlanModal = () => {
+  dataItem.value.aim = ""
+  dataItem.value.educationName = ""
+
   addUpdateDialog.value = true
+  btnTask.value = false
 }
 /* add modal end */
+
+
+/* addDataTask start */
+
+
+const AddDataTask = (item) => {
+  dataTask.value.push(item)
+  dataItem.value.taskName = ""
+  console.log(dataTask);
+}
+/* addDataTask end */
+
+/* update modal start */
+const updateEducationalPlanModal = (item) => {
+  Object.assign(dataItem.value, item)
+  addUpdateDialog.value = true
+}
+/* update modal end */
+
+/* update modal start */
+const detailEducationalPlanModal = (item) => {
+  Object.assign(dataItem.value, item)
+  detailDialog.value = true
+
+}
+/* update modal end */
+
+
+
+
+/* editDataTask start */
+var taskNameId = ref()
+const EditDataTaskModel = (index, item) => {
+  btnTask.value = true
+  editAddBtn.value = "edit"
+  console.log(item);
+  dataItem.value.taskName = item
+  taskNameId = index
+
+}
+/* addDataTask end */
+
+
+/* EditDataTask start */
+const EditDataTask = (item) => {
+  //alert(taskNameId)
+  //alert(dataTask.value[taskNameId])
+  dataTask.value[taskNameId] = item
+  editAddBtn.value = "add"
+
+}
+/* EditDataTask end */
+
+
+/* DeleteDataTask start */
+const DeleteDataTask = (index) => {
+  if (index !== -1) {
+    dataTask.value.splice(index, 1);
+  }
+
+}
+/* DeleteDataTask end */
+
 
 /* delete modal start */
 var deleteDialog = ref(false)
@@ -196,12 +404,7 @@ const deleteEducationalPlanModal = (item) => {
 /* delete modal end */
 
 
-/* update modal start */
-const updateEducationalPlanModal = (item) => {
-  Object.assign(dataItem.value, item)
-  addUpdateDialog.value = true
-}
-/* update modal end */
+
 
 </script>
 <style scoped lang="scss">
@@ -212,5 +415,29 @@ const updateEducationalPlanModal = (item) => {
 
 .input-group {
   margin-bottom: 5px;
+}
+
+.aim-width {
+  width: 100px;
+}
+
+.add-edit-task {
+  border: 1px solid #ccc;
+  padding: 5px;
+}
+
+.bg-light {
+
+  background: #d7d7d7;
+}
+
+.data-task-alert {
+  border: 1px solid #d12424;
+  padding: 20px;
+  margin-top: 5px;
+  border-radius: 5px;
+  background: #d12424;
+  color: white;
+  opacity: 0.7;
 }
 </style>
