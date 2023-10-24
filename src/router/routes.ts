28491d36dad5b9121e3a7b53738517b644695da2
bugradostt <bugradostt@gmail.com>
2023-10-24 11:35:04 +0300

    modal dizayn

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -73,6 +73,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: "", component: () => import('pages/EducationalPlanning.vue') }]
   },
+  {
+    path: '/workplace-service-agreements',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: "", component: () => import('pages/WorkplaceServiceAgreements.vue') }]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
