dc30cadf39d42fdc8bd812e53e2d42cf71353d07
bugradostt <bugradostt@gmail.com>
2023-10-25 15:47:37 +0300

    sm screen fix value

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -73,6 +73,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: "", component: () => import('pages/EducationalPlanning.vue') }]
   },
+  {
+    path: '/shift-definition',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: "", component: () => import('pages/ShiftDefinition.vue') }]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
