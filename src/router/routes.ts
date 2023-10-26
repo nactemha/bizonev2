a04a7cf24e1bde5f4aa6d6b75ddd82b09f1db32f
bugradostt <bugradostt@gmail.com>
2023-10-26 15:55:03 +0300

    page dizayn to write

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -73,6 +73,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: "", component: () => import('pages/EducationalPlanning.vue') }]
   },
+  {
+    path: '/section-directory',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: "", component: () => import('pages/SectionDirectory.vue') }]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
